dc3e79b11a0bbf74e293859578cbdf9ffa9492e8
Jodi <zody.kyozue@gmail.com>
2020-04-03 16:00:33 +0700

    level next

--- database/migrations/2019_10_29_085224_camunda_form.php
+++ database/migrations/2019_10_29_085224_camunda_form.php
@@ -27,12 +27,8 @@ class CamundaForm extends Migration
             $table->text('field_select_query')->nullable();
             $table->json('field_meta')->nullable();
             $table->string('type')->default(\Laravolt\Workflow\Enum\FormType::MAIN_FORM)->comment('See '.\Laravolt\Workflow\Enum\FormType::class);
-            $table->string('called_element')->nullable();
             $table->timestamps();
-            $table->unique('process_definition_key');
-            $table->unique('form_name');
-            $table->unique('field_name');
-            $table->unique('called_element');
+            $table->unique(['process_definition_key', 'form_name', 'field_name'], 'unique_field_per_form');
         });
     }
 
